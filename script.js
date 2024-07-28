b6b95851423fe1e43064bf68d998ba6f50f0e54f
SandroAptsiauri <apciaurisandro15@gmail.com>
2024-07-28 16:19:32 +0400

    skillwill homework4

---
+++ script.js
@@ -0,0 +1,29 @@
+// 1.
+// function replaceValue(string, valueToReplace, valueToReplaceWith) {
+//   return string.split(valueToReplace).join(valueToReplaceWith);
+// }
+// let originalString = "Hello world";
+// let newString = replaceValue(originalString, "world", "js");
+// console.log(newString);
+
+// 2.
+// function upperCase(string) {
+//   return string
+//     .split(" ")
+//     .map((sentence) => sentence.toUpperCase())
+//     .join(" ");
+// }
+// console.log(upperCase("sandro apciauri"));
+
+// 3.
+
+// function sortUsersByAge(users) {
+//     return users.sort((a, b) => a.age - b.age);
+//   }
+//   let users = [
+//     { name: 'Lasha', age: 30 },
+//     { name: 'Saba', age: 20 }
+//   ];
+  
+//   let sortedUsers = sortUsersByAge(users);
+//   console.log(sortedUsers);
